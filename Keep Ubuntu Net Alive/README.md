2aa58f89bbcfb9f5c1ae335ce734bc4e1e691eb7
Prabhakar Bikkaneti <prabhakar.9885@gmail.com>
2017-02-28 15:06:32 +0530

    Init

---
+++ Keep Ubuntu Net Alive/README.md
@@ -0,0 +1,1 @@
+The Bash script keeps pinging the google.com so that the connection will not be disconned due to the inactivity.
---
+++ Keep Ubuntu Net Alive/main.sh
@@ -0,0 +1,5 @@
+# https://ubuntuforums.org/showthread.php?t=922036
+while [ 1 ]; do
+    ping -c1 www.google.com
+    sleep 1m;
+done;
